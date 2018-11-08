ed3d1c86309b1d0ea280b32617b0d99ebc0a6a81
Unknown <ttrinkle@ucmerced.edu>
2018-11-07 17:54:32 -0800

    only 7 queries left boi (untested)

--- phase2.sql
+++ phase2.sql
@@ -7,10 +7,11 @@
 -- 3) Count how many ordered pizzas that have mushrooms are in each type of pizza at Pizza Factory
     SELECT COUNT(*) FROM store, Orders, entree, toppings WHERE st_orderkey = o_orderkey AND o_entree = e_key AND e_name LIKE "pizza" AND st_name = "Pizza Factory";
 
--- 4) For each customer, which drink is most ordered
+-- 4) For each store, which drink is least ordered (which has the most stock)
+    SELECT st_name, d_brand FROM store, drink WHERE d_storekey = st_storeID GROUP BY st_name HAVING MAX(d_stock);
 
 -- 5) For each customer that Dominos delivers to, find their order (entree, drink, side)
-    SELECT e_name, d_name, s_name FROM entree, side, drink, Orders, store, Delivery WHERE order_id = st_orderkey AND st_orderkey = o_orderkey AND o_custkey = c_custkey AND o_drink = d_key AND o_entree = e_key AND o_side = s_key AND st_name = "Dominos" GROUP BY c_name;
+    SELECT e_name, d_brand, s_name FROM entree, side, drink, Orders, store, Delivery WHERE order_id = st_orderkey AND st_orderkey = o_orderkey AND o_custkey = c_custkey AND o_drink = d_key AND o_entree = e_key AND o_side = s_key AND st_name = "Dominos" GROUP BY c_name;
 
 -- 6) Take the inventory of each side, entree ingredient, and drink for each store that offers tea
     SELECT COUNT(d_stock), COUNT(s_stock), COUNT(t_stock), COUNT(cr_stock), COUNT(sc_stock) FROM drinks, sides, toppings, (SELECT st_storeID AS id FROM store, drink WHERE d_type = "tea" AND d_storekey = st_storeID) as q WHERE d_storekey = q.id AND s_storekey = q.id AND t_storekey = q.id GROUP BY q.id;
@@ -30,9 +31,10 @@
 -- 11) Pizza Villa has started to sell Cactus Cooler and Pizza Palace has dropped it. Update the table to reflect this change
     UPDATE drink SET d_storekey = 7 WHERE d_brand = "Cactus Cooler";
 
--- 12) 
+-- 12) Find the topping with the least stock for each store
+    SELECT st_name, t_name FROM topping, store WHERE t_storekey = st_storeID GROUP BY st_name HAVING MIN(t_stock);
 
--- 13)
+-- 13) 
 
 -- 14)
 
