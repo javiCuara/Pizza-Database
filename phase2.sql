268353269a2089a575265529230a8e3db9ba6775
Unknown <ttrinkle@ucmerced.edu>
2018-11-07 18:34:05 -0800

    fixed all queries except 7 because it doesnt want to return anything idk

--- phase2.sql
+++ phase2.sql
@@ -11,13 +11,13 @@
     SELECT st_name, d_brand FROM store, drink WHERE d_storekey = st_storekey GROUP BY st_name HAVING MAX(d_stock);
 
 -- 5) For each customer that Dominos delivers to, find their order (entree, drink, side)
-    SELECT e_name, d_brand, s_name FROM entree, side, drink, Orders, store, Delivery WHERE order_id = st_orderkey AND st_orderkey = o_orderkey AND o_custkey = c_custkey AND o_drink = d_key AND o_entree = e_key AND o_side = s_key AND st_name = "Dominos" GROUP BY c_name;
+    SELECT e_name, d_brand, s_name FROM Customer, entree, sides, drink, Orders, store, Delivery WHERE order_id = st_orderkey AND st_orderkey = o_orderkey AND o_custkey = c_key AND o_drink = d_key AND o_entree = e_key AND o_side = s_key AND st_name = "Dominos" GROUP BY c_name;
 
 -- 6) Take the inventory of each side, entree ingredient, and drink for each store that offers tea
-    SELECT COUNT(d_stock), COUNT(s_stock), COUNT(t_stock), COUNT(cr_stock), COUNT(sc_stock) FROM drinks, sides, toppings, (SELECT st_storekey AS id FROM store, drink WHERE d_type = "tea" AND d_storekey = st_storekey) as q WHERE d_storekey = q.id AND s_storekey = q.id AND t_storekey = q.id GROUP BY q.id;
+    SELECT q.name, COUNT(d_stock), COUNT(s_stock), COUNT(t_stock) FROM drink, sides, toppings, (SELECT st_storekey AS id, st_name AS name FROM store, drink WHERE d_type = "tea" AND d_storekey = st_storekey) as q WHERE d_storekey = q.id AND s_storekey = q.id AND t_storekey = q.id GROUP BY q.id;
 
--- 7) Find the customer(s) who orderd from pizza hut and asked for a pizza with thin crust
-    SELECT c_name FROM Customer, Orders, store, entree, crust WHERE c_custkey = o_orderkey AND o_orderkey = st_orderkey AND st_name = "Pizza Hut" AND o_entree = e_key AND e_ingredients LIKE "thin crust";
+-- 7) Find the customer(s) who ordered from pizza hut and asked for a pizza with thin crust
+    SELECT c_name FROM Customer, Orders, store, entree WHERE c_key = o_custkey AND o_orderkey = st_orderkey AND st_name = "Pizza Hut" AND o_entree = e_key AND e_ingredients LIKE "Thin Cust";
 
 -- 8) List the stores that carry more than 2 types of drinks
     SELECT st_name FROM (SELECT COUNT(DISTINCT d_type) AS numdrinks FROM drink, store WHERE st_storekey = d_storekey) AS q, store GROUP BY st_name HAVING q.numdrinks > 2;
@@ -32,7 +32,7 @@
     UPDATE drink SET d_storekey = 7 WHERE d_brand = "Cactus Cooler";
 
 -- 12) Find the topping with the least stock for each store
-    SELECT st_name, t_name FROM topping, store WHERE t_storekey = st_storekey GROUP BY st_name HAVING MIN(t_stock);
+    SELECT st_name, t_name FROM toppings, store WHERE t_storekey = st_storekey GROUP BY st_name HAVING MIN(t_stock);
 
 -- 13) 
 
